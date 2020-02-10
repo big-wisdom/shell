8640cdc86439d407bc92059503c1870d739659a0
Eli Hermann <Eli_Hermann@hotmail.com>
2020-02-09 20:47:59 -0700

    Runs External Commands, might report standard error

--- src/Shell.java
+++ src/Shell.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 class Shell {
@@ -6,13 +9,49 @@ class Shell {
 
     public static void main(String[] args) {
         while(true){
-            prompt();
+            String userInput = prompt();
+
+            if(userInput == "exit") break; // If quit then quit
+
+            runExternalCommand(userInput); // Try to execute external command
+
+            // try to run built in command
+            // Execute command
+            // End timer and add time to CPU time
+
+            // If the command is invalid
         }
     }
 
-    private static void prompt(){
-        System.out.print("["+workingDirectory+"] ");
-        String response = scanner.nextLine();
-        System.out.println(response);
+    private static String prompt(){
+        System.out.print("["+workingDirectory+"]: ");
+        return scanner.nextLine();
+    }
+
+    private static void runExternalCommand(String args) {
+        try {
+            String[] userInput = args.split(" ");
+            ProcessBuilder processBuilder = new ProcessBuilder(userInput[0]);
+            String s;
+            BufferedReader br;
+
+            // Start timer
+            // Start Process
+            Process process = processBuilder.start();
+            process.waitFor();
+            // Check exit value
+            System.out.println("Eli's Here");
+            if(process.exitValue() != 0) {
+                br = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            }
+            // Print output
+            while ((s = br.readLine()) != null)
+                System.out.println(s);
+
+        } catch(IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
